53a096544f43698d8531780a6c5ce673001e37e1
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-05-13 14:28:36 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.A, function () {
+    jokalari.move(-1)
+})
+input.onButtonPressed(Button.AB, function () {
+    jokalari.move(1)
+})
+input.onButtonPressed(Button.B, function () {
+    jokalari.move(1)
+})
+let jokalari: game.LedSprite = null
+jokalari = game.createSprite(2, 2)
+basic.forever(function () {
+    basic.pause(randint(1000, 1500))
+})
