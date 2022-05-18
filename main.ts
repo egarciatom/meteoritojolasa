c1d7ee89fa8af1c55ae41483d5cb810082342862
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-05-18 14:27:43 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     jokalari.move(-1)
 })
-input.onButtonPressed(Button.AB, function () {
-    jokalari.move(1)
-})
 input.onButtonPressed(Button.B, function () {
     jokalari.move(1)
 })
+let meteorito: game.LedSprite = null
 let jokalari: game.LedSprite = null
-jokalari = game.createSprite(2, 2)
+jokalari = game.createSprite(2, 4)
 basic.forever(function () {
     basic.pause(randint(1000, 1500))
+    meteorito = game.createSprite(randint(0, 4), 0)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(200)
+        meteorito.change(LedSpriteProperty.Y, 1)
+    }
+    if (meteorito.isTouching(jokalari)) {
+        game.gameOver()
+    } else {
+        meteorito.delete()
+    }
 })
